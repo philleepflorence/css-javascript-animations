a1f30a58a6668f950f28d6fab149c75c0d9765ad
Philleep Florence <email@philleep.com>
2013-04-25 10:38:42 -0300

    Update animate.js

--- animate.js
+++ animate.js
@@ -6,7 +6,7 @@ var Animate = function()
       isTransitions = Modernizr.csstransitions,
       transitionEndEvents = 'webkitTransitionEnd transitionend oTransitionEnd MSTransitionEnd transitionend';
 
-  var animate = function(object, cssProperties, duration, callback) 
+  this.animate = function(object, cssProperties, duration, callback) 
   {
     duration = duration || speed;
   
